3fb51ae9ff6a1188c931ff853ac615abd46a129b
Dania <27072003danya@gmail.com>
2023-03-04 16:19:17 +0200

    add new file Content with component and interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Title from './Title'
-
-const Content = () => {
-    return (
-        <React.Fragment>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Blanditiis aliquam quae nulla ipsa nesciunt hic fuga quaerat
-                ullam veritatis? Dignissimos repudiandae officia obcaecati,
-                corrupti hic error dolor inventore nesciunt velit?
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
-                consequuntur, reprehenderit temporibus qui eum maxime
-                repellendus eius animi quaerat possimus non molestiae
-                exercitationem harum sequi suscipit laborum omnis eaque fugit.
-            </p>
-        </React.Fragment>
-    )
-}
+import Content from './Content'
 
 const App = () => {
     return (
         <>
             <Title title="React" year={2023} />
             <Title title="App" year={2022} />
-            <Content />
+            <Content text1="Hello world text1" text2="Hello world text2" />
         </>
     )
 }
