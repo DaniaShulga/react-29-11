0fe949bc65fb79f8a495239db38d284314d67bce
Dania <27072003danya@gmail.com>
2023-03-10 20:50:26 +0200

    третий способ добавления логики кнопкам счётчика для товаров

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -26,13 +26,13 @@ class ProductListItem extends Component<Props, State> {
         count: 1,
     }
 
-    onIncrementClick() {
+    onIncrementClick = () => {
         this.setState((prevState) => ({
             count: prevState.count + 1,
         }))
     }
 
-    onDecrementClick() {
+    onDecrementClick = () => {
         this.setState((prevState) => ({
             count: prevState.count - 1,
         }))
@@ -59,7 +59,7 @@ class ProductListItem extends Component<Props, State> {
                     <div className="product-quantity">
                         <Button
                             variant="outlined"
-                            onClick={() => this.onDecrementClick()}
+                            onClick={this.onDecrementClick}
                         >
                             -
                         </Button>
